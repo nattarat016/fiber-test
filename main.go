710dd675a7b51b385dc37c2e82d0acac7cdeeaa5
nattarat016 <nattaratthepphoy@gmail.com>
2024-01-24 20:31:19 +0700

    midleware

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
 
 type Book struct {
@@ -12,13 +20,41 @@ type Book struct {
 
 var books []Book
 
-func main() {
+func checkMiddleware(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
 
-	app := fiber.New()
+	claims := user.Claims.(jwt.MapClaims)
+
+	if claims["role"] != "admin" {
+		return fiber.ErrUnauthorized
+
+	}
+
+	return c.Next()
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("load .env error")
+	}
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	books = append(books, Book{ID: 1, Title: "firts", Author: "dat"})
 	books = append(books, Book{ID: 2, Title: "secon", Author: "nom"})
 	books = append(books, Book{ID: 3, Title: "tird", Author: "kol"})
+
+	app.Post("/login", login)
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+	}))
+
+	app.Use(checkMiddleware)
 
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
@@ -26,6 +62,10 @@ func main() {
 	app.Put("/books/:id", updateBook)
 	app.Delete("books/:id", deleteBook)
 	app.Post("/uploade", uplodeFile)
+
+	app.Get("/test-html", testHTML)
+
+	app.Get("/config", getEnv)
 
 	app.Listen(":8080")
 }
@@ -44,4 +84,66 @@ func uplodeFile(c *fiber.Ctx) error {
 	}
 
 	return c.SendString("uplode complete")
+}
+
+func testHTML(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{
+		"Name": "World",
+	})
+}
+
+func getEnv(c *fiber.Ctx) error {
+
+	secret := os.Getenv("SECRET")
+
+	if secret == "" {
+		return c.JSON(fiber.Map{
+			"secret": "defaultsecret",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"secret": secret,
+	})
+}
+
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var memberUser = User{
+	Email:    "user@example.com",
+	Password: "password123",
+}
+
+func login(c *fiber.Ctx) error {
+	user := new(User)
+
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	if user.Email != memberUser.Email || user.Password != memberUser.Password {
+		return fiber.ErrUnauthorized
+	}
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["email"] = user.Email
+	claims["role"] = "admin"
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token
+	t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Login success",
+		"t":       t,
+	})
 }
